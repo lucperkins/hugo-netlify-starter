c9e67627814963e6ea6c21cef883d695c86731b1
lucperkins <lucperkins@gmail.com>
2019-12-11 10:44:18 -0800

    Add basic components (navbar, footer, etc.)
    Signed-off-by: lucperkins <lucperkins@gmail.com>

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+const navbarBurger = () => {
+  const burger = $(".navbar-burger"),
+    menu = $(".navbar-menu");
+
+  menu.click(() => {
+    [burger, menu].forEach((el) => el.toggleClass('is-active'));
+  });
+}
+
+$(() => {
+  console.log("Welcome to the CNCF's Hugo + Netlify starter");
+});
---
+++ content/_index.md
@@ -0,0 +1,4 @@
+---
+---
+
+Not a full-fledged theme or anything. Just a starter pack for people that build lots of websites!
